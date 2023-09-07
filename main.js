69627c0e785ee3ed41aac1581f02f1933de05c04
guyguzman <guy.guzman.public@gmail.com>
2023-09-06 23:56:20 -0700

    20230906-2356

---
+++ main.js
@@ -0,0 +1,56 @@
+import "./style.css";
+import * as THREE from "three";
+
+drawSphere();
+
+function drawSphere() {
+  const scene = new THREE.Scene();
+  const geometry = new THREE.SphereGeometry(3, 64, 64);
+  const material = new THREE.MeshStandardMaterial({ color: "#ff0000" });
+
+  const mesh = new THREE.Mesh(geometry, material);
+  scene.add(mesh);
+
+  const light = new THREE.PointLight("#ffffff", 200);
+  light.position.set(0, 10, 10);
+  scene.add(light);
+
+  const camera = new THREE.PerspectiveCamera(45, 800 / 600, 0.1, 100);
+  camera.position.z = 20;
+  scene.add(camera);
+
+  const canvas = document.querySelector(".webgl");
+  const renderer = new THREE.WebGLRenderer({ canvas });
+  renderer.setSize(800, 600);
+  renderer.render(scene, camera);
+  console.log("Hello Three.js");
+}
+
+function drawThreeBezierCurve2D() {
+  const curveCubic = new THREE.CubicBezierCurve(
+    new THREE.Vector2(-10, 0),
+    new THREE.Vector2(-5, 15),
+    new THREE.Vector2(20, 15),
+    new THREE.Vector2(10, 0)
+  );
+  const points = curveCubic.getPoints(50);
+  const spacedPoints = curveCubic.getSpacedPoints(50);
+}
+
+function drawThreeBezierCurve3D() {
+  const curveQuadratic = new THREE.QuadraticBezierCurve3(
+    new THREE.Vector3(0, 921, 0),
+    new THREE.Vector3(921, 921, 0),
+    new THREE.Vector3(921, 0, 0)
+  );
+
+  const curveCubic = new THREE.CubicBezierCurve3(
+    new THREE.Vector3(-10, 0, 0),
+    new THREE.Vector3(-5, 15, 0),
+    new THREE.Vector3(20, 15, 0),
+    new THREE.Vector3(10, 0, 0)
+  );
+
+  const spacedPoints = curveCubic.getSpacedPoints(50);
+  console.log(spacedPoints);
+}
